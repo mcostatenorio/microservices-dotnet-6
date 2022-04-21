c289eb04fdcdc092e4efa00ee82f06c56840d1b1
Milton Tenório <miltontenoriodacosta@outlook.com>
2022-04-21 10:31:06 -0300

    Criação API Person

---
+++ RestWithASPNet/RestWithASPNet/Program.cs
@@ -0,0 +1,22 @@
+using RestWithASPNet.Services;
+using RestWithASPNet.Services.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
