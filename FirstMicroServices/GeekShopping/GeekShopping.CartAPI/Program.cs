08a10a98329c1b5e50baeb1d256ac5af428faf0f
Milton Tenório <miltontenoriodacosta@outlook.com>
2022-05-17 09:21:15 -0300

    Validacao caso o cupom seja alterado

--- FirstMicroServices/GeekShopping/GeekShopping.CartAPI/Program.cs
+++ FirstMicroServices/GeekShopping/GeekShopping.CartAPI/Program.cs
@@ -23,10 +23,14 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddScoped<ICouponRepository, CouponRepository>();
 
 builder.Services.AddSingleton<IRabbitMQMessageSender, RabbitMQMessageSender>();
 
 builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+
+builder.Services.AddHttpClient<ICouponRepository, CouponRepository>(s => s.BaseAddress =
+    new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
 
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer("Bearer", options =>
